5c94c768a9ca61b0efbf5ee05643297b79095c62
rafi.arya <rafi.arya@sucofindo.co.id>
2025-04-13 19:59:26 +0700

    adding swagger

--- swagger.ts
+++ swagger.ts
@@ -24,7 +24,7 @@ const doc = {
       url: "https://opensource.org/licenses/MIT",
     },
   },
-  host: "localhost:3000", // ✅ use host + basePath in Swagger 2.0
+  host: "myfish.agiricatech.com", // ✅ use host + basePath in Swagger 2.0
   schemes: ["http", "https"],
   components: {
     schemas: schemas.definitions,
